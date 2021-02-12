baa92bb153059246341a05c9ee47453fe3167197
mrbigass <wetsand.wfs@gmail.com>
2021-02-12 12:16:56 +0900

    match

--- struct_destruct/src/main.rs
+++ struct_destruct/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    struct Foo {
+        x: (u32, u32),
+        y: u32,
+    }
+
+    let foo = Foo { x: (1, 2), y: 3 };
+
+    match foo {
+        Foo { x: (1, b), y } => println!("first of x is 1, b = {}, y = {}", b, y),
+        Foo { y: 2, x: i } => println!("{:?}", i),
+        Foo { y, .. } => println!("we don't care about x, just for y: {}", y),
+    }
 }
