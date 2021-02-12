eae068488cbc565fc513328b4cfb3dc612e6de18
mrbigass <wetsand.wfs@gmail.com>
2021-02-12 09:47:22 +0900

    no_guide

--- conditional_branch/src/main.rs
+++ conditional_branch/src/main.rs
@@ -1,16 +1,14 @@
 fn main() {
     let mut count = 0u32;
-    println!("Let's count until infinity!");
 
     loop {
         count += 1;
 
         if count == 3 {
             println!("three");
-            continue;
+        } else {
+            println!("{}", count);
         }
-
-        println!("{}", count);
 
         if count == 5 {
             break;
