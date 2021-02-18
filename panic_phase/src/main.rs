0760ecc97d6f2fa5f9b8eda72ea3542322a9247f
mrbigass <wetsand.wfs@gmail.com>
2021-02-18 22:57:32 +0900

    princess causes panic

--- panic_phase/src/main.rs
+++ panic_phase/src/main.rs
@@ -1,3 +1,10 @@
+fn give_princess(gift: &str) {
+    if gift == "snake" {
+        panic!("AAA!!!")
+    }
+    println!("I Love{}s!!!!", gift);
+}
 fn main() {
-    println!("Hello, world!");
+    give_princess("teddy bear");
+    give_princess("snake");
 }
